3560e5d1b9c8165d594288fa4f4b64c26f38fbd8
Alex Yeropudov <oleksandr.yeropudov@gmail.com>
2019-04-16 16:49:53 +0300

    fix return type

--- src/StatsClient.php
+++ src/StatsClient.php
@@ -37,7 +37,7 @@ class StatsClient
             'id' => $requestId
         ];
 
-        $this->sendRequest($body);
+        return $this->sendRequest($body);
     }
 
     private function sendRequest(array $body)
